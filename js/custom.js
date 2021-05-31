5572f23d5c864eb37e0a4764a02bc0f7cd7d32e1
yangyuelin <joe14030@hotmail.com>
2021-05-31 20:26:43 +0800

    移除無用js

--- js/custom.js
+++ js/custom.js
@@ -303,22 +303,22 @@ let mainNavLinks = document.querySelectorAll(".site-menu li a");
 let lastId;
 let cur = [];
 
-window.addEventListener("scroll", event => {
-    let fromTop = window.scrollY;
+// window.addEventListener("scroll", event => {
+//     let fromTop = window.scrollY;
 
-    mainNavLinks.forEach(link => {
-        let section = document.querySelector(link.hash);
+//     mainNavLinks.forEach(link => {
+//         let section = document.querySelector(link.hash);
 
-        if (
-            section.offsetTop <= fromTop &&
-            section.offsetTop + section.offsetHeight > fromTop
-        ) {
-            link.classList.add("active");
-        } else {
-            link.classList.remove("active");
-        }
-    });
-});
+//         if (
+//             section.offsetTop <= fromTop &&
+//             section.offsetTop + section.offsetHeight > fromTop
+//         ) {
+//             link.classList.add("active");
+//         } else {
+//             link.classList.remove("active");
+//         }
+//     });
+// });
 
 
 
